01ab7acd347e6c1b388cc43f1bebd336493b2b44
Rob Lester <rblestr@gmail.com>
2015-09-21 14:50:48 +0100

    Added testing frameworks to package.json
    * mocha
    * chai
    * sinon

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect
+
+describe('test case', function() {
+    describe('example function', function() {
+        it('should work with chai', function() {
+            expect(40).to.equal(40)
+        });
+    });
+});
